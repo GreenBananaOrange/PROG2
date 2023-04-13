ea216b6aa1c6c427e71f0756443a7717fa402c27
gabrielpenkert <gabrielpenkert@gmail.com>
2023-04-13 12:58:45 +0200

    Lecture Lesson: generics-package added and the GenericsTest.java class

---
+++ src/com/gabrielpenkert/prog2/lectureScripts/generics/GenericsTest.java
@@ -0,0 +1,27 @@
+package com.gabrielpenkert.prog2.lectureScripts.generics;
+
+import java.util.*;
+
+public class GenericsTest {
+
+    public static boolean hasEvenSize (Collection<? extends Number> collection) {
+    return collection.size() % 2 == 0;
+}
+    public static void main (String[] args) {
+        System.out.println(hasEvenSize(new ArrayList<Integer>()));
+        ArrayList<Integer> l = new ArrayList<>();
+        l.add(1);
+        l.add(1);
+        l.add(1);
+        System.out.println(countOccurrences(l , 1));
+    }
+
+    public static int countOccurrences (Collection<?> container, Object element) {
+        int result = 0;
+        for (Object el : container) {
+            if (el.equals(element))
+                result++;
+        }
+        return result;
+    }
+}
