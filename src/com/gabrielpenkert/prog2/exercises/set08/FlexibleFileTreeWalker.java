56ecb7f278382499b1b91fee94265ee9ad9c8591
gabrielpenkert <gabrielpenkert@gmail.com>
2023-05-08 14:21:13 +0200

    exercise 8: aufgabe 4 nicht fertig, aber weit

--- src/com/gabrielpenkert/prog2/exercises/set08/FlexibleFileTreeWalker.java
+++ src/com/gabrielpenkert/prog2/exercises/set08/FlexibleFileTreeWalker.java
@@ -1,8 +1,8 @@
 package com.gabrielpenkert.prog2.exercises.set08;
 
-
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Prints recursively all files which are contained in * the current directory or in sub-directories.
@@ -10,30 +10,45 @@ import java.io.IOException;
  */
 
 public class FlexibleFileTreeWalker {
+    public FileProcessor fileProcessor;
+
     public static void main(String[] args) throws IOException {
-        new FileTreeWalker().walk(".");
+        FlexibleFileTreeWalker.walk(".");
+        // fileProcessor.walk(".");
+        new FlexibleFileTreeWalker.LargeFileFinder();
     }
 
-    public void walk(String pathname) throws IOException { // hier passiert das wesentliche
+    public FlexibleFileTreeWalker (FileProcessor fileProcessor) {
+        this.fileProcessor = fileProcessor;
+    }
+
+    public static void walk(String pathname) throws IOException { // hier passiert das wesentliche
         File[] files = new File(pathname).listFiles(); // alle Dateipfade werden geladen und in Filearray gelegt
         if (files != null) { // unnötig?
             for (File file : files) // fuer alle Dateien im Array
                 if (file.isDirectory()) // Wenn Ziel nur ein Verzeichnis darstellt, dann:
                     walk(file.getCanonicalPath());
                 else // Wenn Ziel tatsaechlich eine Datei ist, dann...
-                    process(file);
+                    FileProcessor.process(file);
         }
-    }
-
-    protected void process(File file) throws IOException {
-        System.out.printf("%-100s%6d%n", file.getCanonicalPath(), file.length()); // ...wird sie hier geprintet
     }
 
     interface FileProcessor {
+        static void process(File file) throws IOException {
+            System.out.printf("%-100s%6d%n", file.getCanonicalPath(), file.length()); // ...wird sie hier geprintet
+        }
+    }
 
-        static void process(File file) {
+    public static class LargeFileFinder {
+        HashMap<String, Integer> sortedPaths = new HashMap<>();
+        int bitCounter = -1;
+
+        public LargeFileFinder () {
 
         }
 
+        public void addElement (String path, int sizeOfFile) {
+            sortedPaths.put(path, sizeOfFile);
+        }
     }
 }
