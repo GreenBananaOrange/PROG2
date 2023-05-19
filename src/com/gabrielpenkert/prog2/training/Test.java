80a7709f85f187587664979328eb5143947859f1
GreenBananaOrange <54152817+GreenBananaOrange@users.noreply.github.com>
2023-05-19 20:41:44 +0200

    tested sth.

---
+++ src/com/gabrielpenkert/prog2/training/Test.java
@@ -0,0 +1,20 @@
+package com.gabrielpenkert.prog2.training;
+
+public class Test {
+
+    public static void main (String[] args) {
+        System.out.println(getInt(1, 4));
+    }
+
+    public static String getInt (int i, int f) {
+        return "zahl3";
+    }
+
+    public static String getInt (int i) {
+        return "zahl2";
+    }
+
+    public static String getInt () {
+        return "zahl1";
+    }
+}
